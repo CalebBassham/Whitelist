e0c967537db3717bf8b5c56ba824acdc5ac859b7
Caleb Bassham <caleb.bassham@gmail.com>
2019-01-12 14:08:47 -0600

    Update to use PluginMessageFormat

--- src/main/java/me/calebbassham/whitelist/WhitelistCmd.java
+++ src/main/java/me/calebbassham/whitelist/WhitelistCmd.java
@@ -1,7 +1,6 @@
 package me.calebbassham.whitelist;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,6 +11,9 @@ import org.bukkit.entity.Player;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static me.calebbassham.pluginmessageformat.PluginMessageFormat.getMainColorPallet;
+import static me.calebbassham.pluginmessageformat.PluginMessageFormat.getPrefix;
+
 public class WhitelistCmd implements CommandExecutor, TabCompleter {
 
     @Override
@@ -20,34 +22,46 @@ public class WhitelistCmd implements CommandExecutor, TabCompleter {
         if (args.length == 1) {
             if (List.of("on", "enable", "true").contains(args[0].toLowerCase())) {
                 Bukkit.setWhitelist(true);
-                sender.sendMessage(ChatColor.AQUA + "Whitelist has been enabled.");
+                sender.sendMessage(getPrefix() + getMainColorPallet().getHighlightTextColor() + "Whitelist" +
+                        getMainColorPallet().getPrimaryTextColor() + " has been " + getMainColorPallet().getValueTextColor() + "enabled" +
+                        getMainColorPallet().getPrimaryTextColor() + ".");
                 return true;
             }
 
             if (List.of("off", "disable", "false").contains(args[0].toLowerCase())) {
                 Bukkit.setWhitelist(false);
-                sender.sendMessage(ChatColor.AQUA + "Whitelist has been disabled.");
+                sender.sendMessage(getPrefix() + getMainColorPallet().getHighlightTextColor() + "Whitelist" +
+                        getMainColorPallet().getPrimaryTextColor() + " has been " + getMainColorPallet().getValueTextColor() + "disabled" +
+                        getMainColorPallet().getPrimaryTextColor() + ".");
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("all") || args[0].equalsIgnoreCase("*")) {
                 Bukkit.getOnlinePlayers().forEach(player -> player.setWhitelisted(true));
-                sender.sendMessage(ChatColor.AQUA + "All players have been added to the whitelist.");
+                sender.sendMessage(getPrefix() + getMainColorPallet().getValueTextColor() + "All players" +
+                        getMainColorPallet() + " have been " + getMainColorPallet().getHighlightTextColor() + "added" +
+                        getMainColorPallet().getPrimaryTextColor() + " to the " + getMainColorPallet().getHighlightTextColor() + "whitelist" +
+                        getMainColorPallet().getPrimaryTextColor() + ".");
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("clear")) {
                 Bukkit.getWhitelistedPlayers().forEach(player -> player.setWhitelisted(false));
-                sender.sendMessage(ChatColor.AQUA + "The whitelist has been cleared.");
+                sender.sendMessage(getPrefix() + "The " + getMainColorPallet().getHighlightTextColor() + "whitelist" +
+                        getMainColorPallet().getPrimaryTextColor() + " has been " + getMainColorPallet().getHighlightTextColor() + "cleared" +
+                        getMainColorPallet().getPrimaryTextColor() + ".");
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("list")) {
                 if (Bukkit.getWhitelistedPlayers().size() == 0) {
-                    sender.sendMessage(ChatColor.AQUA + "Whitelist is empty.");
+                    sender.sendMessage(getPrefix() + getMainColorPallet().getHighlightTextColor() + "Whitelist" +
+                            getMainColorPallet().getPrimaryTextColor() + " is " + getMainColorPallet().getValueTextColor() +
+                            "empty" + getMainColorPallet().getPrimaryTextColor() + ".");
                     return true;
                 }
-                sender.sendMessage(ChatColor.AQUA + Bukkit.getWhitelistedPlayers().stream().map(OfflinePlayer::getName).collect(Collectors.joining(", ")));
+
+                sender.sendMessage(getPrefix() + Bukkit.getWhitelistedPlayers().stream().map(OfflinePlayer::getName).collect(Collectors.joining(", ")));
                 return true;
             }
         }
@@ -55,20 +69,36 @@ public class WhitelistCmd implements CommandExecutor, TabCompleter {
         if (args.length == 2) {
             if (args[0].equalsIgnoreCase("add")) {
                 var target = Bukkit.getOfflinePlayer(args[1]);
-                sender.sendMessage(String.format(ChatColor.AQUA + "%s has been added to the whitelist.", args[1]));
+
+                sender.sendMessage(getPrefix() + getMainColorPallet().getValueTextColor() + getDisplayName(target) + getMainColorPallet().getPrimaryTextColor() +
+                        " has been " + getMainColorPallet().getHighlightTextColor() + "whitelisted" + getMainColorPallet().getPrimaryTextColor() + ".");
+
                 target.setWhitelisted(true);
                 return true;
             }
 
             if (args[0].equalsIgnoreCase("remove")) {
                 var target = Bukkit.getOfflinePlayer(args[1]);
-                sender.sendMessage(String.format(ChatColor.AQUA + "%s has been removed from the whitelist.", args[1]));
+
+                sender.sendMessage(getPrefix() + getMainColorPallet().getValueTextColor() + getDisplayName(target) + getMainColorPallet().getPrimaryTextColor() +
+                        " has been " + getMainColorPallet().getHighlightTextColor() + "unwhitelisted" + getMainColorPallet().getPrimaryTextColor() + ".");
+
                 target.setWhitelisted(false);
                 return true;
             }
         }
 
         return false;
+    }
+
+    private String getDisplayName(OfflinePlayer offlinePlayer) {
+        var player = offlinePlayer.getPlayer();
+
+        if (player != null) {
+            return player.getDisplayName();
+        } else {
+            return offlinePlayer.getName();
+        }
     }
 
     @Override
